b65624f9129d2462b7b52b89ff354ce7c8a069de
vedant-pandey <vedantpandey46@gmail.com>
2025-10-13 16:35:30 +0530

    y24 day3 part2

--- Y24/day3.cpp
+++ Y24/day3.cpp
@@ -11,7 +11,8 @@ __attribute((always_inline)) inline u64 solve(std::string const line) {
     u64 f = 0, s = 0, sum = 0;
     for (int i = 3; i < line.size(); i++) {
         // mul(\d\d\d,\d\d\d)
-        if (line[i] == '(' && line[i - 1] == 'l' && line[i - 2] == 'u' && line[i - 3] == 'm') {
+        if (line[i] == '(' && line[i - 1] == 'l' && line[i - 2] == 'u'
+            && line[i - 3] == 'm') {
             i++;
             while (line[i] >= '0' && line[i] <= '9') {
                 f *= 10;
@@ -40,11 +41,14 @@ __attribute((always_inline)) inline u64 solve(std::string const line) {
     return sum;
 }
 
-__attribute((always_inline)) inline u64 solve2(std::string const line) {
+__attribute((always_inline)) inline u64 solve2(std::string const line, u8& doInst) {
     u64 f = 0, s = 0, sum = 0;
     for (int i = 3; i < line.size(); i++) {
-        // mul(\d\d\d,\d\d\d)
-        if (line[i] == '(' && line[i - 1] == 'l' && line[i - 2] == 'u' && line[i - 3] == 'm') {
+        if (doInst == 1
+            && (
+                // mul(\d\d\d,\d\d\d)
+                line[i - 3] == 'm' && line[i - 2] == 'u' && line[i - 1] == 'l'
+                && line[i] == '(')) {
             i++;
             while (line[i] >= '0' && line[i] <= '9') {
                 f *= 10;
@@ -67,6 +71,17 @@ __attribute((always_inline)) inline u64 solve2(std::string const line) {
             }
             sum += f * s;
             f = s = 0;
+        } else if (line[i - 3] == 'd' && line[i - 2] == 'o'
+                   && line[i - 1] == '(' && line[i] == ')') {
+            // do()
+            doInst = 1;
+        } else if (i >= 6
+                   && (line[i - 6] == 'd' && line[i - 5] == 'o'
+                       && line[i - 4] == 'n' && line[i - 3] == '\''
+                       && line[i - 2] == 't' && line[i - 1] == '('
+                       && line[i] == ')')) {
+            // don't()
+            doInst = 0;
         }
     }
 
@@ -98,8 +113,9 @@ void part2() {
 
     std::string line;
     u64 soln = 0;
+    u8 doInst = 1;
     while (std::getline(inputFile, line)) {
-        soln += solve2(line);
+        soln += solve2(line, doInst);
     }
     inputFile.close();
 
